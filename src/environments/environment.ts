ce5230dc66567307b0d64e65a2ff221439fc72dc
palakgontiya <palak.gontiya28@gmail.com>
2025-03-08 22:06:12 +0530

    firebase added

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDX0Eb0YhKoK6zf-iPhFS4UPLVutFxdFjI",
+        authDomain: "aboutme-65cbc.firebaseapp.com",
+        projectId: "aboutme-65cbc",
+        storageBucket: "aboutme-65cbc.firebasestorage.app",
+        messagingSenderId: "660654487645",
+        appId: "1:660654487645:web:25c8310aef718fe7a1b5b4",
+        measurementId: "G-C6CXKJ8XS9"
+    }
+  };
+
+  
+  
